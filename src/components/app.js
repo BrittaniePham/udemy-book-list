089408de893b5aef3a4659ddee545d16c881867a
Brittanie Pham <brittaniepham@gmail.com>
2018-08-07 16:08:08 -0600

    book detail/active book reducer

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Booklist from '../containers/BookList'
+import BookDetail from '../containers/BookDetail'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Booklist />
+        <BookDetail />
       </div>
     );
   }
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { selectBook } from '../actions/index'
+import { bindActionCreators } from 'redux'
 
 class BookList extends Component {
   renderList() {
     return(
       this.props.books.map((book) => {
         return(
-          <li className="list-group-item" key={book.title}>
+          <li 
+            className="list-group-item" 
+            key={book.title}
+            onClick={() => this.props.selectBook(book)}
+          >
             {book.title}
           </li>
         )
@@ -27,6 +33,10 @@ function mapStateToProps(state) {
   return {
     books: state.books
   }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectBook: selectBook }, dispatch)
 }
 
 export default connect(mapStateToProps)(BookList);
